1b65567061034fea493ce95730dfca74fe6181a7
VictorG10 <victorgbadebo10@gmail.com>
2025-05-20 01:30:23 +0100

    setting up auth context

---
+++ contexts/AuthContext.js
@@ -0,0 +1,62 @@
+import authService from "@/services/authService";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    checkUser();
+  }, []);
+
+  const checkUser = async () => {
+    setLoading(true);
+    const response = await authService.getUser();
+
+    if (response?.error) {
+      setUser(null);
+    } else {
+      setUser(response);
+    }
+    setLoading(false);
+  };
+
+  const login = async (email, password) => {
+    const response = await authService.login(email, password);
+
+    if (response?.error) {
+      return response;
+    }
+
+    await checkUser();
+    return { success: true };
+  };
+
+  const register = async (email, password) => {
+    const response = await authService.register(email, password);
+
+    if (response?.error) {
+      return response;
+    }
+
+    return login(email, password);
+  };
+
+  const logout = async () => {
+    await authService.logout();
+
+    setUser(null);
+
+    await checkUser();
+  };
+
+  return (
+    <AuthProvider.Provider value={{ user, login, register, logout, loading }}>
+      {children}
+    </AuthProvider.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
